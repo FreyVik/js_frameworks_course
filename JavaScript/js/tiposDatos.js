e8cfbac6a95205bae58b7a58d6f02eafd3bd58a5
FreyVik <yonkigamer96@gmail.com>
2021-09-29 22:07:21 +0200

    JavaScript init

---
+++ JavaScript/js/tiposDatos.js
@@ -0,0 +1,18 @@
+"use strict";
+
+var num1 = 7;
+var num2 = 12;
+var operacion = num1 + num2;
+
+console.log(`El resultado de la operacion es: ${operacion}`);
+
+// Tipos de datos
+var numEntero = 40;
+var text = "Hola 'que' tal";
+var trueFalse = false;
+
+var numFalse = "33";
+
+console.log(String(numEntero) + 7);
+
+console.log(typeof numFalse);
