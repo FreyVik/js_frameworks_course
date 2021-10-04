bb0ade98490d0e81909ea111b41aa222845a4063
FreyVik <yonkigamer96@gmail.com>
2021-10-04 20:44:43 +0200

    Sobre mi

--- test_project/js/main.js
+++ test_project/js/main.js
@@ -1,45 +1,46 @@
 $(document).ready(function () {
     //  Slider
-    $('.slider').bxSlider({
-        mode: 'fade',
-        captions: true,
-        slideWidth: 1200,
-        responsive: true,
-        pager: false,
-    });
+    if (window.location.href.indexOf('index') > -1) {
+        $('.slider').bxSlider({
+            mode: 'fade',
+            captions: true,
+            slideWidth: 1200,
+            responsive: true,
+            pager: false,
+        });
 
-    //  Post
-    const posts = [
-        {
-            title: 'Titulo del post 1',
-            date: moment().format('D MMMM YYYY'),
-            content:
-                'Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam ipsum blanditiis corporis, dignissimos dicta reprehenderit nisi velit modi esse officiis neque animi explicabo ab et soluta quasi minima deserunt id at. Cupiditate nobis deserunt similique quos repellat veniam dolore corporis vel non quam dolorum sunt modi, excepturi numquam quisquam dignissimos quasi! Impedit deserunt expedita neque placeat at? Incidunt impedit repellat ullam excepturi, unde error ipsa est omnis nobis porro cumque quam. Facilis dolorum fuga qui, omnis commodi quibusdam soluta officiis nihil tempora saepe vitae sunt neque accusamus dolorem eum recusandae error sequi. Dicta praesentium minus in laudantium nesciunt, quo optio! ',
-        },
-        {
-            title: 'Titulo del post 2',
-            date: moment().format('D MMMM YYYY'),
-            content:
-                'Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam ipsum blanditiis corporis, dignissimos dicta reprehenderit nisi velit modi esse officiis neque animi explicabo ab et soluta quasi minima deserunt id at. Cupiditate nobis deserunt similique quos repellat veniam dolore corporis vel non quam dolorum sunt modi, excepturi numquam quisquam dignissimos quasi! Impedit deserunt expedita neque placeat at? Incidunt impedit repellat ullam excepturi, unde error ipsa est omnis nobis porro cumque quam. Facilis dolorum fuga qui, omnis commodi quibusdam soluta officiis nihil tempora saepe vitae sunt neque accusamus dolorem eum recusandae error sequi. Dicta praesentium minus in laudantium nesciunt, quo optio! ',
-        },
-        {
-            title: 'Titulo del post 3',
-            date: moment().format('D MMMM YYYY'),
-            content:
-                'Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam ipsum blanditiis corporis, dignissimos dicta reprehenderit nisi velit modi esse officiis neque animi explicabo ab et soluta quasi minima deserunt id at. Cupiditate nobis deserunt similique quos repellat veniam dolore corporis vel non quam dolorum sunt modi, excepturi numquam quisquam dignissimos quasi! Impedit deserunt expedita neque placeat at? Incidunt impedit repellat ullam excepturi, unde error ipsa est omnis nobis porro cumque quam. Facilis dolorum fuga qui, omnis commodi quibusdam soluta officiis nihil tempora saepe vitae sunt neque accusamus dolorem eum recusandae error sequi. Dicta praesentium minus in laudantium nesciunt, quo optio! ',
-        },
-        {
-            title: 'Titulo del post 4',
-            date: moment().format('D MMMM YYYY'),
-            content:
-                'Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam ipsum blanditiis corporis, dignissimos dicta reprehenderit nisi velit modi esse officiis neque animi explicabo ab et soluta quasi minima deserunt id at. Cupiditate nobis deserunt similique quos repellat veniam dolore corporis vel non quam dolorum sunt modi, excepturi numquam quisquam dignissimos quasi! Impedit deserunt expedita neque placeat at? Incidunt impedit repellat ullam excepturi, unde error ipsa est omnis nobis porro cumque quam. Facilis dolorum fuga qui, omnis commodi quibusdam soluta officiis nihil tempora saepe vitae sunt neque accusamus dolorem eum recusandae error sequi. Dicta praesentium minus in laudantium nesciunt, quo optio! ',
-        },
-    ];
+        //  Post
+        const posts = [
+            {
+                title: 'Titulo del post 1',
+                date: moment().format('D MMMM YYYY'),
+                content:
+                    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam ipsum blanditiis corporis, dignissimos dicta reprehenderit nisi velit modi esse officiis neque animi explicabo ab et soluta quasi minima deserunt id at. Cupiditate nobis deserunt similique quos repellat veniam dolore corporis vel non quam dolorum sunt modi, excepturi numquam quisquam dignissimos quasi! Impedit deserunt expedita neque placeat at? Incidunt impedit repellat ullam excepturi, unde error ipsa est omnis nobis porro cumque quam. Facilis dolorum fuga qui, omnis commodi quibusdam soluta officiis nihil tempora saepe vitae sunt neque accusamus dolorem eum recusandae error sequi. Dicta praesentium minus in laudantium nesciunt, quo optio! ',
+            },
+            {
+                title: 'Titulo del post 2',
+                date: moment().format('D MMMM YYYY'),
+                content:
+                    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam ipsum blanditiis corporis, dignissimos dicta reprehenderit nisi velit modi esse officiis neque animi explicabo ab et soluta quasi minima deserunt id at. Cupiditate nobis deserunt similique quos repellat veniam dolore corporis vel non quam dolorum sunt modi, excepturi numquam quisquam dignissimos quasi! Impedit deserunt expedita neque placeat at? Incidunt impedit repellat ullam excepturi, unde error ipsa est omnis nobis porro cumque quam. Facilis dolorum fuga qui, omnis commodi quibusdam soluta officiis nihil tempora saepe vitae sunt neque accusamus dolorem eum recusandae error sequi. Dicta praesentium minus in laudantium nesciunt, quo optio! ',
+            },
+            {
+                title: 'Titulo del post 3',
+                date: moment().format('D MMMM YYYY'),
+                content:
+                    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam ipsum blanditiis corporis, dignissimos dicta reprehenderit nisi velit modi esse officiis neque animi explicabo ab et soluta quasi minima deserunt id at. Cupiditate nobis deserunt similique quos repellat veniam dolore corporis vel non quam dolorum sunt modi, excepturi numquam quisquam dignissimos quasi! Impedit deserunt expedita neque placeat at? Incidunt impedit repellat ullam excepturi, unde error ipsa est omnis nobis porro cumque quam. Facilis dolorum fuga qui, omnis commodi quibusdam soluta officiis nihil tempora saepe vitae sunt neque accusamus dolorem eum recusandae error sequi. Dicta praesentium minus in laudantium nesciunt, quo optio! ',
+            },
+            {
+                title: 'Titulo del post 4',
+                date: moment().format('D MMMM YYYY'),
+                content:
+                    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam ipsum blanditiis corporis, dignissimos dicta reprehenderit nisi velit modi esse officiis neque animi explicabo ab et soluta quasi minima deserunt id at. Cupiditate nobis deserunt similique quos repellat veniam dolore corporis vel non quam dolorum sunt modi, excepturi numquam quisquam dignissimos quasi! Impedit deserunt expedita neque placeat at? Incidunt impedit repellat ullam excepturi, unde error ipsa est omnis nobis porro cumque quam. Facilis dolorum fuga qui, omnis commodi quibusdam soluta officiis nihil tempora saepe vitae sunt neque accusamus dolorem eum recusandae error sequi. Dicta praesentium minus in laudantium nesciunt, quo optio! ',
+            },
+        ];
 
-    const postsDiv = $('#posts');
+        const postsDiv = $('#posts');
 
-    posts.forEach((item, index) => {
-        var post = `
+        posts.forEach((item, index) => {
+            var post = `
             <article class="post">
                 <h2>${item.title}</h2>
                 <span class="date">Fecha publicacion ${item.date}</span>
@@ -48,8 +49,9 @@ $(document).ready(function () {
             </article>
         `;
 
-        postsDiv.append(post);
-    });
+            postsDiv.append(post);
+        });
+    }
 
     //  Themes
     const theme = $('#theme');
@@ -75,4 +77,8 @@ $(document).ready(function () {
             500
         );
     });
+
+    if (window.location.href.indexOf('about') > -1) {
+        $('#acordeon').accordion();
+    }
 });
